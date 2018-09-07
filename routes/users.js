03e0fb01b803b2a02e434d8dc16e232b293a5635
Havit_R <havitchoirul.rovik@mitrais.com>
2018-09-08 06:43:50 +0700

    Implement GET for Users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
+const Users = require('../models').User;
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  Users.findAll()
+    .then(result => {
+      const users = result.map(node => {
+        const {
+          id,
+          name,
+          email,
+          phoneNumber,
+          address
+        } = node.get({
+          plain: true
+        });
+
+        return {
+          id,
+          name,
+          email,
+          phoneNumber,
+          address
+        };
+      });
+
+      res.json({
+        users
+      });
+    }).catch(err => {
+      res.status(204).send(err);
+    });
 });
 
 module.exports = router;
