439ebb27e4e6fb19bcd3b1a905096fe310e83d03
Havit_R <havitchoirul.rovik@mitrais.com>
2018-09-10 05:42:57 +0700

    Fix Navbar

--- client/components/header/Header.jsx
+++ client/components/header/Header.jsx
@@ -1,20 +1,42 @@
 import React from "react";
-import { Collapse, Nav, Navbar, NavbarToggler, NavLink } from 'reactstrap';
+import { Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from "react-router-dom";
 
 // The Header creates links that can be used to navigate
 // between routes.
-const Header = () => (
-  <header>
-    <Navbar expand="md">
-      <Nav className="ml-auto" navbar>
-        <NavLink tag={Link} to="/">Home</NavLink>
-        <NavLink tag={Link} to="/user">Add User</NavLink>
-      </Nav>
-      <NavbarToggler />
-      <Collapse isOpen={false} navbar />
-    </Navbar>
-  </header>
-);
+class Header extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  toggle = () => this.setState({ isOpen: !this.state.isOpen });
+
+  render() {
+    return (
+      <header>
+        <Navbar light expand="md">
+          <NavbarBrand href="/">
+            <span role="img" aria-label="Checkmark">👨‍💼</span>
+          </NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink tag={Link} to="/">Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} to="/user">Add User</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </header>
+    );
+  }
+}
 
 export default Header;
